2d8ada9cdb64ac948672b93872a73982afca725e
Giannis Koutsoukidis <john.koutsoukidis@outlook.com>
2021-04-05 19:30:35 +0200

    Fixed packaging issue with absorb damage effect

--- Private/EffectAbsorbDamage.cpp
+++ Private/EffectAbsorbDamage.cpp
@@ -2,14 +2,14 @@
 #include "C_Character.h"
 #include "HealthComponent.h"
 
-inline void UEffectAbsorbDamage::Initialize_Implementation(UStatusBase* ParentStatus)
+void UEffectAbsorbDamage::Initialize_Implementation(UStatusBase* ParentStatus)
 {
 	Super::Initialize_Implementation(ParentStatus);
 	if (Status->TargetActor)
 		Status->TargetActor->Health->Absorbs.Add(this);
 }
 
-inline void UEffectAbsorbDamage::OnExpired_Implementation()
+void UEffectAbsorbDamage::OnExpired_Implementation()
 {
 	if (Status->TargetActor)
 		Status->TargetActor->Health->Absorbs.Remove(this);

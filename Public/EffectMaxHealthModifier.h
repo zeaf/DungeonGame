afcbb5304d42241ba40135cbcd54070d768e5d64
Giannis Koutsoukidis <john.koutsoukidis@outlook.com>
2021-04-05 19:34:52 +0200

    Added Max Health modifier effect

---
+++ Public/EffectMaxHealthModifier.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "CombatAttribute.h"
+#include "C_Character.h"
+#include "EffectBase.h"
+#include "HealthComponent.h"
+#include "EffectMaxHealthModifier.generated.h"
+
+UCLASS(BlueprintType, EditInlineNew, DefaultToInstanced, ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class HELENAPLAYGROUND_API UEffectMaxHealthModifier : public UEffectBase
+{
+
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Modifier")
+		StatModifier Modifier = StatModifier::Multiplicative;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Modifier")
+		float Amount = 0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Modifier")
+		bool HealableOnly;
+
+	void Initialize_Implementation(UStatusBase* ParentStatus) override;
+
+	void OnExpired_Implementation() override;
+};
+
+inline void UEffectMaxHealthModifier::Initialize_Implementation(UStatusBase* ParentStatus)
+{
+	Super::Initialize_Implementation(ParentStatus);
+	AC_Character* Pawn = Status->TargetActor;
+	if (Pawn)
+	{
+		FCombatAttribute Health = HealableOnly ? Pawn->Health->MaxHealableHealth : Pawn->Health->MaxHealth;
+		Health.AddEffect(GetUniqueID(), Modifier, Amount);
+	}
+}
+
+inline void UEffectMaxHealthModifier::OnExpired_Implementation()
+{
+	AC_Character* Pawn = Status->TargetActor;
+	if (Pawn)
+	{
+		FCombatAttribute Health = HealableOnly ? Pawn->Health->MaxHealableHealth : Pawn->Health->MaxHealth;
+		Health.RemoveEffect(GetUniqueID(), Modifier);
+	}
+}

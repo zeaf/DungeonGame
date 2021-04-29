14ed731a6e87864dae4b086098e55d87a2d758ef
Giannis Koutsoukidis <john.koutsoukidis@outlook.com>
2021-04-29 17:07:21 +0200

    Initial Active Ability class implementation

--- Private/ActiveAbilityBase.cpp
+++ Private/ActiveAbilityBase.cpp
@@ -3,3 +3,55 @@
 
 #include "ActiveAbilityBase.h"
 
+#include "C_Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+
+void UActiveAbilityBase::BeginPlay()
+{
+	CanCastWhileMoving = CastTime == 0 || CanCastWhileMoving;
+	AbilityCastingComponent = Caster->AbilityCasting;
+}
+
+void UActiveAbilityBase::BPRemoveResource_Implementation()
+{
+	if (Cost > 0)
+		Caster->RemoveResource(Cost);
+}
+
+void UActiveAbilityBase::ServerStartCooldown_Implementation(const float Duration, const bool IsGCD)
+{
+	if (Duration > 0)
+	{
+		CooldownReady = false;
+		GetWorld()->GetTimerManager().SetTimer(CooldownTimer, this,  &UActiveAbilityBase::ResetCooldown, Duration, false);
+		ClientCooldown(Duration, IsGCD);
+	}
+}
+
+void UActiveAbilityBase::ResetCooldown()
+{
+	CooldownReady = true;
+	ElapsedCD = 0;
+}
+
+bool UActiveAbilityBase::CastConditions_Implementation()
+{
+	return CheckResourceAvailability() && CooldownReady && CheckCastableWhileMoving();
+}
+
+bool UActiveAbilityBase::CheckResourceAvailability_Implementation()
+{
+	return true;
+}
+
+bool UActiveAbilityBase::CheckCastableWhileMoving()
+{
+	return CanCastWhileMoving || Caster->GetCharacterMovement()->GetLastUpdateVelocity().Equals(FVector(0, 0, 0), 1);
+}
+
+void UActiveAbilityBase::StartGCD(const float Time)
+{
+	if (GetWorld()->GetTimerManager().GetTimerRemaining(CooldownTimer) < Time)
+		ServerStartCooldown(Time, true);
+}
--- Public/ActiveAbilityBase.h
+++ Public/ActiveAbilityBase.h
@@ -6,12 +6,110 @@
 #include "AbilityBase.h"
 #include "ActiveAbilityBase.generated.h"
 
-/**
- * 
- */
+
+class UAbilityCastingComponent;
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FCastSuccessDelegate);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FCastFailedDelegate);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FCastInterruptedDelegate);
+
 UCLASS()
 class HELENAPLAYGROUND_API UActiveAbilityBase : public UAbilityBase
 {
 	GENERATED_BODY()
+
+protected:
+	virtual void BeginPlay() override;
 	
+public:
+	UPROPERTY(BlueprintAssignable, BlueprintCallable)
+		FCastSuccessDelegate OnCastSuccess;
+
+	UPROPERTY(BlueprintAssignable, BlueprintCallable)
+		FCastFailedDelegate OnCastFailed;
+	
+	UPROPERTY(BlueprintAssignable, BlueprintCallable)
+		FCastInterruptedDelegate OnCastInterrupted;
+	
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		bool CanCastWhileMoving;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		bool CanCastWhileCasting;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		bool IsChanneled;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		bool CooldownReady;
+	
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		float Cost;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		float CastTime;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		float Cooldown = 1.f;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		float GCD = 1.5f;
+	
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		float ElapsedCD;
+
+	UPROPERTY(BlueprintReadOnly)
+	UAbilityCastingComponent* AbilityCastingComponent;
+	
+	UPROPERTY()
+	FTimerHandle CooldownTimer;
+	
+	UFUNCTION(BlueprintNativeEvent)
+	void CastSuccess();
+	virtual void CastSuccess_Implementation() {}
+
+	UFUNCTION(BlueprintNativeEvent)
+	void ServerOnBeginCast();
+	virtual void ServerOnBeginCast_Implementation() {}
+
+	UFUNCTION(BlueprintNativeEvent)
+	void ServerOnFinishedCast();
+	virtual void ServerOnFinishedCast_Implementation() {}
+
+	UFUNCTION(NetMulticast, Reliable)
+	void MulticastRemoveResource();
+	void MulticastRemoveResource_Implementation() { BPRemoveResource(); }
+
+	UFUNCTION(NetMulticast, Reliable)
+	void MulticastAbilityCast();
+	void MulticastAbilityCast_Implementation() { BPMulticastAbilityCast(); }
+
+	UFUNCTION(BlueprintNativeEvent, meta = (DisplayName = MulticastAbilityCast))
+	void BPMulticastAbilityCast();
+	virtual void BPMulticastAbilityCast_Implementation() {}
+	
+	UFUNCTION(BlueprintNativeEvent, meta=(DisplayName = RemoveResource))
+	void BPRemoveResource();
+	virtual void BPRemoveResource_Implementation();
+
+	UFUNCTION(Server, Reliable)
+	void ServerStartCooldown(const float Duration, const bool IsGCD);
+	void ServerStartCooldown_Implementation(const float Duration, const bool IsGCD);
+
+	UFUNCTION(BlueprintNativeEvent)
+	void ClientCooldown(const float Duration, const bool IsGCD);
+	virtual void ClientCooldown_Implementation(const float Duration, const bool IsGCD) {};
+
+	void ResetCooldown();
+
+	UFUNCTION(BlueprintNativeEvent)
+	UPARAM(DisplayName = "Can Cast") bool CastConditions();
+	bool CastConditions_Implementation();
+
+	UFUNCTION(BlueprintNativeEvent)
+	UPARAM(DisplayName = "Resource Available") bool CheckResourceAvailability();
+	bool CheckResourceAvailability_Implementation();
+
+	bool CheckCastableWhileMoving();
+
+	void StartGCD(const float Time);
 };

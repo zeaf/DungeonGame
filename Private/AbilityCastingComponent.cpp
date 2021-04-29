291cd8c645240893249ebfc2bb16153c63a6e21b
Giannis Koutsoukidis <john.koutsoukidis@outlook.com>
2021-04-29 17:06:59 +0200

    Added first networked functions for casting abilties

--- Private/AbilityCastingComponent.cpp
+++ Private/AbilityCastingComponent.cpp
@@ -3,6 +3,9 @@
 
 #include "AbilityCastingComponent.h"
 
+#include "ActiveAbilityBase.h"
+#include "C_Character.h"
+
 // Sets default values for this component's properties
 UAbilityCastingComponent::UAbilityCastingComponent()
 {
@@ -10,7 +13,7 @@ UAbilityCastingComponent::UAbilityCastingComponent()
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
 	SetIsReplicatedByDefault(true);
-	// ...
+	Character = Cast<AC_Character>(GetOwner());
 }
 
 
@@ -34,5 +37,62 @@ void UAbilityCastingComponent::TickComponent(float DeltaTime, ELevelTick TickTyp
 
 void UAbilityCastingComponent::ServerCastTime_Implementation()
 {
+	if (CurrentlyCastingAbility->IsChanneled)
+	{
+		CurrentlyCastingAbility->MulticastRemoveResource();
+		CurrentlyCastingAbility->MulticastAbilityCast();	
+	}
+
+	CurrentlyCastingAbility->ServerOnBeginCast();
+
+	GetWorld()->GetTimerManager().SetTimer(CastingTimer, this, &UAbilityCastingComponent::SuccessfulCastSequence, CurrentlyCastingAbility->CastTime, false);
+}
+
+void UAbilityCastingComponent::SuccessfulCastSequence()
+{
+	if (CurrentlyCastingAbility->IsChanneled)
+	{
+		CurrentlyCastingAbility->MulticastAbilityCast();
+		CurrentlyCastingAbility->MulticastRemoveResource();
+	}
+
+	CurrentlyCastingAbility->ServerOnFinishedCast();
+	
+}
+
+void UAbilityCastingComponent::ServerAttemptToCast_Implementation(UActiveAbilityBase* Ability)
+{
+	if (!IsCasting || CanCastWhileCasting)
+	{
+		if (Ability->CastConditions())
+		{
+			TriggerGCD(Character->GetCombatAttributeValue(CombatAttributeName::CooldownRate) * Ability->GCD);
+
+			if (Ability->CastTime > 0)
+			{
+				ClientCastbar(Ability);
+				if (!Ability->CanCastWhileCasting)
+				{
+					IsCasting = true;
+					CanCastWhileMoving = true;
+					CurrentlyCastingAbility = Ability;
+					ServerCastTime();
+				}
+			}
+			else
+				SuccessfulCastSequence();
+		}
+	}
+}
+
+void UAbilityCastingComponent::ClientCastbar_Implementation(const UActiveAbilityBase* Ability)
+{
+	
+}
+
+void UAbilityCastingComponent::TriggerGCD(const float Time)
+{
+	for (UActiveAbilityBase* Ability : ActiveAbilities)
+		Ability->StartGCD(Time);
 }
 
--- Public/AbilityCastingComponent.h
+++ Public/AbilityCastingComponent.h
@@ -7,6 +7,8 @@
 #include "AbilityCastingComponent.generated.h"
 
 
+class AC_Character;
+class UActiveAbilityBase;
 class UAbilityBase;
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class HELENAPLAYGROUND_API UAbilityCastingComponent : public UActorComponent
@@ -21,6 +23,9 @@ protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
+private:
+	AC_Character* Character;
+
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
@@ -32,7 +37,7 @@ public:
 		bool CanCastWhileCasting;
 
 	UPROPERTY(BlueprintReadWrite, VisibleAnywhere)
-		UAbilityBase* CurrentlyCastingAbility;
+		UActiveAbilityBase* CurrentlyCastingAbility;
 
 	UPROPERTY(BlueprintReadWrite, EditAnywhere)
 		bool IsCasting;
@@ -40,6 +45,27 @@ public:
 	UPROPERTY(BlueprintReadWrite)
 		FTimerHandle CastingTimer;
 
+	UPROPERTY(EditAnywhere, Instanced, BlueprintReadwrite, SimpleDisplay, Category = "Abilities", Meta = (DisplayName = "Active Abilities", ExposeFunctionCategories = "Abilities", AllowPrivateAccess = "true"))
+	TArray<UActiveAbilityBase*> ActiveAbilities;
+
+	UPROPERTY(EditAnywhere, Instanced, BlueprintReadwrite, SimpleDisplay, Category = "Abilities", Meta = (DisplayName = "Passive Ability", ExposeFunctionCategories = "Abilities", AllowPrivateAccess = "true"))
+	UAbilityBase* PassiveAbility;
+
+
 	UFUNCTION(Server, Reliable)
-		void ServerCastTime();
+	void ServerCastTime();
+	void ServerCastTime_Implementation();
+
+	UFUNCTION()
+	void SuccessfulCastSequence();
+
+	UFUNCTION(Server, Reliable)
+	void ServerAttemptToCast(UActiveAbilityBase* Ability);
+	void ServerAttemptToCast_Implementation(UActiveAbilityBase* Ability);
+
+	UFUNCTION(Client, Reliable)
+	void ClientCastbar(const UActiveAbilityBase* Ability);
+	void ClientCastbar_Implementation(const UActiveAbilityBase* Ability);
+	
+	void TriggerGCD(const float Time);
 };

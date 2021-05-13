019590bdaabc2e72e09bc7f47ba391defde66a8d
Giannis Koutsoukidis <john.koutsoukidis@outlook.com>
2021-05-13 06:16:42 +0200

    Added initialize tooltip on aura widget

--- Private/UI/Widget_AuraWrapper.cpp
+++ Private/UI/Widget_AuraWrapper.cpp
@@ -4,9 +4,11 @@
 #include "UI/Widget_AuraWrapper.h"
 
 #include "StatusBase.h"
+#include "UI/Widget_AuraTooltip.h"
 #include "UI/Widget_SpellIcon.h"
 
 void UWidget_AuraWrapper::InitializeAura(UStatusBase* Status, UTexture2D* StatusIcon, float Duration)
 {
 	Icon->InitializeIcon(Status, StatusIcon, Duration);
+	Tooltip->InitializeTooltip(Status);
 }
